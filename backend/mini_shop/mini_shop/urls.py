d2f89cd73843ba994a5b4a3ee6e90d6276baecae
paul <paulbrowne2@gmail.com>
2022-01-21 15:56:48 +0000

    Initial commit

---
+++ backend/mini_shop/mini_shop/urls.py
@@ -0,0 +1,14 @@
+from pipes import Template
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/payment/', include('payment.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'))
+]
