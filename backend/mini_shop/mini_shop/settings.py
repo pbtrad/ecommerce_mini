dbf7d9d443bc168d9969d8beed40b6aeee8675cb
paul <paulbrowne2@gmail.com>
2022-01-21 16:12:05 +0000

    updated env variables

--- backend/mini_shop/mini_shop/settings.py
+++ backend/mini_shop/mini_shop/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&#+@dn)ga5!j3plx!ar4ir%di4xas*4ti8!(^((4h#7l$cv3i#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
